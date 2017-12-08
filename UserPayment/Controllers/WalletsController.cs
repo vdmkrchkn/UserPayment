629317442d173680504ab521cd6f191d83f743d5
vdmkrchkn <vdmkrchkn@gmail.com>
2017-12-09 02:25:16 +0300

    сделал поиск по счетам асинхронным

--- UserPayment/Controllers/WalletsController.cs
+++ UserPayment/Controllers/WalletsController.cs
@@ -1,8 +1,6 @@
 ﻿using NLog;
 using System;
-using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using UserPayment.Models;
